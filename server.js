a2c515d4e61eb9027fc454760c15f89a738ed17f
james-salafatinos <jamessalafatinos@outlook.com>
2021-04-11 13:57:31 -0500

    great progress. Working on integrated POC

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 const port = 8080;
 const bodyParser = require("body-parser");
 
+const { spawn } = require("child_process");
+
 //Required for environment variables
 require("dotenv").config();
 
@@ -17,7 +19,22 @@ app.get("/", (req, res) => {
   console.log("User entry to site...");
   res.status(200);
   res.sendFile(__dirname + "/src/network");
-  console.log(req.query);
+  console.log("User's desired wikipedia page...: ", req.query);
+
+  // spawn new child process to call the python script
+  // spawn new child process to call the python script
+  let x = "and joy to the world";
+  const process = spawn("python", ["./utils/main.py", req.query.new_graph]);
+
+  // collect data from script
+  process.stdout.on("data", (data) => {
+    console.log(data.toString());
+  });
+
+  // in close event we are sure that stream is from child process is closed
+  process.on("close", (code) => {
+    console.log(`child process close all stdio with code ${code}`);
+  });
 });
 
 //Express config to enable the model serving of static javascript file
